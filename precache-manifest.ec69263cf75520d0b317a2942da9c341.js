567889df22427632060853513c68ab042f922905
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-04-04 19:05:19 +0530

    Updates

---
+++ precache-manifest.ec69263cf75520d0b317a2942da9c341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ed71af4732285a8b4b3",
+    "url": "/react-solitr/static/css/main.8344e3d2.chunk.css"
+  },
+  {
+    "revision": "2ed71af4732285a8b4b3",
+    "url": "/react-solitr/static/js/main.db844f8a.chunk.js"
+  },
+  {
+    "revision": "c68b4a2a2131c8ba2008",
+    "url": "/react-solitr/static/js/runtime~main.3809054a.js"
+  },
+  {
+    "revision": "00394ac03c792cf42865",
+    "url": "/react-solitr/static/js/2.9bba1761.chunk.js"
+  },
+  {
+    "revision": "3675da19ef0e21ac04dcbdc82004ddb5",
+    "url": "/react-solitr/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-solitr/precache-manifest.35c5dc5312e10073e5e49e2e34096bf5.js"
+  "/react-solitr/precache-manifest.ec69263cf75520d0b317a2942da9c341.js"
 );
 
 workbox.clientsClaim();
