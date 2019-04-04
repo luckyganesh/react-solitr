298fec71b889f4fbf842fb1d0ccffc92bc04ce49
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-04-04 09:54:50 +0530

    Updates

---
+++ precache-manifest.377f6b809f2d82b0bec22d44c823ad44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e9d2e54e3495d109659",
+    "url": "/react-solitr/static/css/main.409fced1.chunk.css"
+  },
+  {
+    "revision": "4e9d2e54e3495d109659",
+    "url": "/react-solitr/static/js/main.5fd43185.chunk.js"
+  },
+  {
+    "revision": "c68b4a2a2131c8ba2008",
+    "url": "/react-solitr/static/js/runtime~main.3809054a.js"
+  },
+  {
+    "revision": "9c7f21f851ab04be587b",
+    "url": "/react-solitr/static/js/2.35d176dc.chunk.js"
+  },
+  {
+    "revision": "b2e2c045df640605271666348133265e",
+    "url": "/react-solitr/index.html"
+  }
+];
